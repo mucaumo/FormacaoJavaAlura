4a0fe0d40705e720da3f73b00747165e83c4cc54
Murilo Caumo Murgia <murilo.caumo.murgia@gmail.com>
2024-03-18 19:39:53 -0300

    Vendo um pouco sobre Strings e Concatenação

--- OrientaçãoObjetos/src/App.java
+++ OrientaçãoObjetos/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8) /3;
+        System.out.println(media);
+        
+        String sinopse;
+        sinopse = "Filme de aventura com galã dos anos 80";
+        System.out.println(sinopse);
+
+        int classificacao;
+        classificacao =  (int) (media /2);
+        System.out.println(classificacao);
     }
 }
