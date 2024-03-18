1e3cd2dc6e677fd50e8b0f8dd7374da9dd023bfa
Murilo Caumo Murgia <murilo.caumo.murgia@gmail.com>
2024-03-18 17:07:49 -0300

    Primeiro Olá Mundo

---
+++ OrientaçãoObjetos/src/PrimeiraClasse.java
@@ -0,0 +1,5 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+    } 
+}
